3ce73acad85fb10b3325ebfbbdb2c1d75b0bd981
Chralt98 <chralt98@gmail.com>
2022-06-08 09:00:24 +0200

    Revert "update to polkadot-v0.9.22 (#745)"
    This reverts commit f709ed62262435b3ad80482d309e3575625d1e5b.

--- oracle/rpc/src/lib.rs
+++ oracle/rpc/src/lib.rs
@@ -1,36 +1,33 @@
 use std::sync::Arc;
 
 use codec::Codec;
-use jsonrpsee::{
-	core::{async_trait, RpcResult},
-	proc_macros::rpc,
-	types::error::{CallError, ErrorObject},
-};
+use jsonrpc_core::{Error as RpcError, ErrorCode, Result};
+use jsonrpc_derive::rpc;
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
 use sp_runtime::{generic::BlockId, traits::Block as BlockT};
 
+pub use self::gen_client::Client as OracleClient;
 pub use orml_oracle_rpc_runtime_api::OracleApi as OracleRuntimeApi;
 
-#[rpc(client, server)]
+#[rpc]
 pub trait OracleApi<BlockHash, ProviderId, Key, Value> {
-	#[method(name = "oracle_getValue")]
-	fn get_value(&self, provider_id: ProviderId, key: Key, at: Option<BlockHash>) -> RpcResult<Option<Value>>;
-	#[method(name = "oracle_getAllValues")]
-	fn get_all_values(&self, provider_id: ProviderId, at: Option<BlockHash>) -> RpcResult<Vec<(Key, Option<Value>)>>;
+	#[rpc(name = "oracle_getValue")]
+	fn get_value(&self, provider_id: ProviderId, key: Key, at: Option<BlockHash>) -> Result<Option<Value>>;
+	#[rpc(name = "oracle_getAllValues")]
+	fn get_all_values(&self, provider_id: ProviderId, at: Option<BlockHash>) -> Result<Vec<(Key, Option<Value>)>>;
 }
 
-/// Provides RPC methods to query oracle value.
+/// A struct that implements the [`OracleApi`].
 pub struct Oracle<C, B> {
-	/// Shared reference to the client.
 	client: Arc<C>,
 	_marker: std::marker::PhantomData<B>,
 }
 
 impl<C, B> Oracle<C, B> {
-	/// Creates a new instance of the `Oracle` helper.
+	/// Create new `Oracle` with the given reference to the client.
 	pub fn new(client: Arc<C>) -> Self {
-		Self {
+		Oracle {
 			client,
 			_marker: Default::default(),
 		}
@@ -41,20 +38,18 @@ pub enum Error {
 	RuntimeError,
 }
 
-impl From<Error> for i32 {
-	fn from(e: Error) -> i32 {
+impl From<Error> for i64 {
+	fn from(e: Error) -> i64 {
 		match e {
 			Error::RuntimeError => 1,
 		}
 	}
 }
 
-#[async_trait]
-impl<C, Block, ProviderId, Key, Value> OracleApiServer<<Block as BlockT>::Hash, ProviderId, Key, Value>
-	for Oracle<C, Block>
+impl<C, Block, ProviderId, Key, Value> OracleApi<<Block as BlockT>::Hash, ProviderId, Key, Value> for Oracle<C, Block>
 where
 	Block: BlockT,
-	C: ProvideRuntimeApi<Block> + HeaderBackend<Block> + Send + Sync + 'static,
+	C: Send + Sync + 'static + ProvideRuntimeApi<Block> + HeaderBackend<Block>,
 	C::Api: OracleRuntimeApi<Block, ProviderId, Key, Value>,
 	ProviderId: Codec,
 	Key: Codec,
@@ -65,17 +60,16 @@ where
 		provider_id: ProviderId,
 		key: Key,
 		at: Option<<Block as BlockT>::Hash>,
-	) -> RpcResult<Option<Value>> {
+	) -> Result<Option<Value>> {
 		let api = self.client.runtime_api();
-		let at = BlockId::hash(at.unwrap_or_else(|| self.client.info().best_hash));
-
-		api.get_value(&at, provider_id, key).map_err(|e| {
-			CallError::Custom(ErrorObject::owned(
-				Error::RuntimeError.into(),
-				"Unable to get value.",
-				Some(e.to_string()),
-			))
-			.into()
+		let at = BlockId::hash(at.unwrap_or(
+			// If the block hash is not supplied assume the best block.
+			self.client.info().best_hash,
+		));
+		api.get_value(&at, provider_id, key).map_err(|e| RpcError {
+			code: ErrorCode::ServerError(Error::RuntimeError.into()),
+			message: "Unable to get value.".into(),
+			data: Some(format!("{:?}", e).into()),
 		})
 	}
 
@@ -83,17 +77,16 @@ where
 		&self,
 		provider_id: ProviderId,
 		at: Option<<Block as BlockT>::Hash>,
-	) -> RpcResult<Vec<(Key, Option<Value>)>> {
+	) -> Result<Vec<(Key, Option<Value>)>> {
 		let api = self.client.runtime_api();
-		let at = BlockId::hash(at.unwrap_or_else(|| self.client.info().best_hash));
-
-		api.get_all_values(&at, provider_id).map_err(|e| {
-			CallError::Custom(ErrorObject::owned(
-				Error::RuntimeError.into(),
-				"Unable to get all values.",
-				Some(e.to_string()),
-			))
-			.into()
+		let at = BlockId::hash(at.unwrap_or(
+			// If the block hash is not supplied assume the best block.
+			self.client.info().best_hash,
+		));
+		api.get_all_values(&at, provider_id).map_err(|e| RpcError {
+			code: ErrorCode::ServerError(Error::RuntimeError.into()),
+			message: "Unable to get all values.".into(),
+			data: Some(format!("{:?}", e).into()),
 		})
 	}
 }
--- tokens/rpc/src/lib.rs
+++ tokens/rpc/src/lib.rs
@@ -2,11 +2,8 @@
 use std::sync::Arc;
 
 use codec::Codec;
-use jsonrpsee::{
-	core::{async_trait, Error as JsonRpseeError, RpcResult},
-	proc_macros::rpc,
-	types::error::{CallError, ErrorCode, ErrorObject},
-};
+use jsonrpc_core::{Error as RpcError, ErrorCode, Result};
+use jsonrpc_derive::rpc;
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
 use sp_rpc::number::NumberOrHex;
@@ -15,23 +12,23 @@ use sp_runtime::{
 	traits::{Block as BlockT, MaybeDisplay},
 };
 
+pub use self::gen_client::Client as TokensClient;
 pub use orml_tokens_rpc_runtime_api::TokensApi as TokensRuntimeApi;
 
-#[rpc(client, server)]
+#[rpc]
 pub trait TokensApi<BlockHash, CurrencyId, Balance> {
-	#[method(name = "tokens_queryExistentialDeposit")]
-	fn query_existential_deposit(&self, currency_id: CurrencyId, at: Option<BlockHash>) -> RpcResult<NumberOrHex>;
+	#[rpc(name = "tokens_queryExistentialDeposit")]
+	fn query_existential_deposit(&self, currency_id: CurrencyId, at: Option<BlockHash>) -> Result<NumberOrHex>;
 }
 
-/// Provides RPC methods to query existential deposit of currency.
+/// A struct that implements the [`TokensApi`].
 pub struct Tokens<C, P> {
-	/// Shared reference to the client.
 	client: Arc<C>,
 	_marker: std::marker::PhantomData<P>,
 }
 
 impl<C, P> Tokens<C, P> {
-	/// Creates a new instance of the `Tokens` helper.
+	/// Create new `Tokens` with the given reference to the client.
 	pub fn new(client: Arc<C>) -> Self {
 		Self {
 			client,
@@ -46,46 +43,43 @@ pub enum Error {
 	RuntimeError,
 }
 
-impl From<Error> for i32 {
-	fn from(e: Error) -> i32 {
+impl From<Error> for i64 {
+	fn from(e: Error) -> i64 {
 		match e {
 			Error::RuntimeError => 1,
 		}
 	}
 }
 
-#[async_trait]
-impl<C, Block, CurrencyId, Balance> TokensApiServer<<Block as BlockT>::Hash, CurrencyId, Balance> for Tokens<C, Block>
+impl<C, Block, CurrencyId, Balance> TokensApi<<Block as BlockT>::Hash, CurrencyId, Balance> for Tokens<C, Block>
 where
 	Block: BlockT,
-	C: ProvideRuntimeApi<Block> + HeaderBackend<Block> + Send + Sync + 'static,
+	C: Send + Sync + 'static + ProvideRuntimeApi<Block> + HeaderBackend<Block>,
 	C::Api: TokensRuntimeApi<Block, CurrencyId, Balance>,
-	Balance: Codec + MaybeDisplay + Copy + TryInto<NumberOrHex> + Send + Sync + 'static,
+	Balance: Codec + MaybeDisplay + Copy + TryInto<NumberOrHex> + std::fmt::Debug,
 	CurrencyId: Codec,
 {
 	fn query_existential_deposit(
 		&self,
 		currency_id: CurrencyId,
 		at: Option<<Block as BlockT>::Hash>,
-	) -> RpcResult<NumberOrHex> {
+	) -> Result<NumberOrHex> {
 		let api = self.client.runtime_api();
-		let at = BlockId::hash(at.unwrap_or_else(|| self.client.info().best_hash));
+		let at = BlockId::hash(at.unwrap_or_else(||
+			// If the block hash is not supplied assume the best block.
+			self.client.info().best_hash));
 
-		let balance = api.query_existential_deposit(&at, currency_id).map_err(|e| {
-			CallError::Custom(ErrorObject::owned(
-				Error::RuntimeError.into(),
-				"Unable to query existential deposit.",
-				Some(e.to_string()),
-			))
+		let balance = api.query_existential_deposit(&at, currency_id).map_err(|e| RpcError {
+			code: ErrorCode::ServerError(Error::RuntimeError.into()),
+			message: "Unable to query existential_deposit.".into(),
+			data: Some(format!("{:?}", e).into()),
 		});
 
 		let try_into_rpc_balance = |value: Balance| {
-			value.try_into().map_err(|_| {
-				JsonRpseeError::Call(CallError::Custom(ErrorObject::owned(
-					ErrorCode::InvalidParams.code(),
-					format!("{} doesn't fit in NumberOrHex representation", value),
-					None::<()>,
-				)))
+			value.try_into().map_err(|_| RpcError {
+				code: ErrorCode::InvalidParams,
+				message: format!("{} doesn't fit in NumberOrHex representation", value),
+				data: None,
 			})
 		};
 
